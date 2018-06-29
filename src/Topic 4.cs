ee7e79223c64970bc8416af43026a2b236e34a27
shreesharao <sshreesha4@gmail.com>
2018-06-29 18:28:27 +0530

    Extension method related points are covered

--- src/Topic 4.cs
+++ src/Topic 4.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static Csharp60New.MyMath;
 using static System.String;
+using static Csharp60New.Topic_4Extension; //this is not required
 
 namespace Csharp60New
 {
@@ -13,11 +14,19 @@ namespace Csharp60New
     {
         public void Entry()
         {
-            Console.WriteLine(new MyMath().Divide(1, 2)); 
+            Console.WriteLine(new MyMath().Divide(1, 2));
 
             Console.WriteLine(Add(1, 2));
             Console.WriteLine(Multiply(1, 2));
             Console.WriteLine(IsNullOrEmpty("Shreesha"));
+
+            //extension method call
+            //Subtract();       //it is not possible to call it as static method
+            new Topic_4().Subtract(10, 5);
+
+            //Divide method must be called like this
+            new MyMath().Divide(10, 5);
+
         }
 
     }
@@ -27,6 +36,12 @@ namespace Csharp60New
         public static int Add(int num1, int num2) => num1 + num2;
         public static int Multiply(int num1, int num2) => num1 * num2;
         public int Divide(int num1, int num2) => num1 / num2;
+    }
+
+    //extension method
+    static class Topic_4Extension
+    {
+        public static int Subtract(this Topic_4 topic_4, int num1, int num2) => num1 - num2;
     }
 
 
